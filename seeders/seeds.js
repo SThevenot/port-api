0a95a18c8e171279cce740f1ffcae77854bd43a5
SThevenot <sktmh007@gmail.com>
2022-12-21 11:08:41 -0600

    established mongodb connection file

---
+++ seeders/seeds.js
@@ -0,0 +1,16 @@
+/** @format */
+const db = require("../config/connection");
+const { Project, Skill } = require("../models");
+const projectSeeds = require("./projectSeeds.json");
+const skillSeeds = require("./skillSeeds.json");
+
+db.once("open", async () => {
+  await Skill.deleteMany({});
+  await Skill.create(skillSeeds);
+  await Project.deleteMany({});
+
+  await Project.create(projectSeeds);
+
+  console.log("all done!");
+  process.exit(0);
+});
