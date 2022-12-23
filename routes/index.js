ac908b5c04aac02961e6c2ee908d0cc48b319c5b
SThevenot <sktmh007@gmail.com>
2022-12-23 14:22:16 -0600

    get requiest for indexhtml

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,4 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
-router.use((req, res) => res.send('Wrong route!'));
-
 module.exports = router;
--- server.js
+++ server.js
@@ -3,17 +3,21 @@
 const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
+const path = require('path');
 
 const PORT = 3001;
 const app = express();
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`running on port localhost:${PORT}!`);
-    });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`running on port localhost:${PORT}!`);
   });
+});
